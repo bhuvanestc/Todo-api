49c93be82b3a64f92356f23ae0975f9592002f2b
bhuvan <bhuvanapraap@gmail.com>
2023-11-01 16:33:40 +0100

    task repo methods

--- src/main/java/se/lexicon/g46todoapi/repository/TaskRepository.java
+++ src/main/java/se/lexicon/g46todoapi/repository/TaskRepository.java
@@ -1,19 +1,40 @@
 package se.lexicon.g46todoapi.repository;
 
+import org.aspectj.apache.bcel.classfile.LocalVariable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import se.lexicon.g46todoapi.domain.entity.Task;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
   // todo: select tasks by title
+  @Query("SELECT t FROM Task t WHERE t.title = :title")
+  List<Task> findByTitle(String title);
   // todo: select tasks by person id
-  // todo: select tasks by status
+  @Query("SELECT t FROM Task t WHERE t.person = :personId")
+  List<Task> findByPersonId(Long personId);
+
+    // todo: select tasks by status
+    @Query("SELECT t FROM Task t WHERE t.done = :status")
+    List<Task> findByStatus(String status);
   // todo: select tasks by date between start and end
+   @Query("SELECT t FROM Task t WHERE t.deadline BETWEEN :startDate AND :endDate")
+    List<Task> findByDueDateBetween(LocalDate startDate, LocalDate endDate);
   // todo: select tasks by deadline
+  @Query("SELECT t FROM Task t WHERE t.deadline < :deadline")
+  List<Task> findByDueDateBefore(LocalDate deadline);
   // todo: select all un-assigned tasks
+  @Query("SELECT t FROM Task t WHERE t.person IS NULL")
+  List<Task> findByPersonIsNull();
   // todo: select all un-finished tasks
-  // add more as needed...
+  @Query("SELECT t FROM Task t WHERE not t.done")
+  List<Task> findByStatusNotIn(List<String> finishedStatuses);
+
 
 }
